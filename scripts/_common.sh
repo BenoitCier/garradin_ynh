add1e9cda7ea8224ed9eeffd8f2b4f54d65803a1
Robles Rodolphe <rodolphe.robles@sfr.fr>
2021-03-17 13:00:30 +0100

    try debug update condition for old versions

--- scripts/_common.sh
+++ scripts/_common.sh
@@ -4,10 +4,13 @@
 # COMMON VARIABLES
 #=================================================
 # dependencies used by the app
+if ynh_compare_current_package_version --comparison lt --version 0.9.8.1~ynh1; then
+YNH_PHP_VERSION="7.2"
+else
 YNH_PHP_VERSION="7.3"
+fi
 
 extra_php_dependencies="php${YNH_PHP_VERSION}-sqlite3"
-
 #=================================================
 # PERSONAL HELPERS
 #=================================================
